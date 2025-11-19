499a13eae2e7db88115ca34509013fc972c864f5
neyha06 <kadamneha2002@gmail.com>
2025-11-19 11:41:50 +0530

    Upload Form & File Uploader Component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -27,7 +27,7 @@ export default function Home() {
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
-      <section className="main-section">z
+      <section className="main-section">
         <div className="page-heading py-60">
           <h1>Track Your Application & Resume Rating</h1>
           <h2>Review your submissions and check AI-powered feedback.</h2>
